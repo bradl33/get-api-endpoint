0f17a7578f11f604b42949e7e0c52d817692a438
bradl33 <39657175+bradl33@users.noreply.github.com>
2022-10-28 14:18:10 +0300

    Added @CrossOrigin to HngMemberController.java

--- src/main/java/com/bradl3y/getapiendpoint/HngMemberController.java
+++ src/main/java/com/bradl3y/getapiendpoint/HngMemberController.java
@@ -1,9 +1,11 @@
 package com.bradl3y.getapiendpoint;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class HngMemberController {
 
